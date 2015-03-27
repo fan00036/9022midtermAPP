80ab881751a50617a9f9a0f65bfc881c5e5f3a7b
Jinhong Fan <helen@HELENs-MacBook-Pro.local>
2015-03-27 15:35:56 -0400

    222
    2222

---
+++ 9022midterm/loveU/9022midterm/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,18 @@
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes.dex : \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\com\edumedia\fan00036\helen9022midterm\BuildConfig.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\com\edumedia\fan00036\helen9022midterm\CordovaApp.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\com\edumedia\fan00036\helen9022midterm\R$attr.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\com\edumedia\fan00036\helen9022midterm\R$drawable.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\com\edumedia\fan00036\helen9022midterm\R$string.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\com\edumedia\fan00036\helen9022midterm\R$xml.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\com\edumedia\fan00036\helen9022midterm\R.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessor$WhereOptions.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessor.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactAccessorSdk5.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactInfoDTO.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$1.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$2.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$3.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager$4.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes\org\apache\cordova\contacts\ContactManager.class \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\dexedLibs\classes-dc3a30c2cce67b13cc4bc78838f33950.jar \
