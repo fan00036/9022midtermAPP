18858da8ef2be57598b2251e4d6296a79443f6ad
Jinhong Fan <helen@HELENs-MacBook-Pro.local>
2015-03-27 15:33:54 -0400

    ye
    ye

---
+++ 9022midterm/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\AndroidManifest.xml : \
+C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\AndroidManifest.xml \
