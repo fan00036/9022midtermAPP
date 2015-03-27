7b95138e5d50166c1b8128fd008054895d6fd207
Jinhong Fan <helen@HELENs-MacBook-Pro.local>
2015-03-27 15:36:30 -0400

    finally!!
    finally!!

--- 9022midterm/loveU/9022midterm/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\CordovaApp.ap_ \
-C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\ant-build\classes.dex \
-C:\wamp\www\mad9022\cordova4.1\helenmidtermAPP\9022midterm\platforms\android\CordovaLib\ant-build\classes.jar \
